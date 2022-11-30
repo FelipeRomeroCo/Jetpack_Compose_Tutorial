ac7ba0fc138fbc858891e1bbc9536b7f573d27a5
Felipe Romero <tmfromero@gmail.com>
2022-11-30 14:13:24 -0300

    Fin de la clase #4, Animaciones, en donde se detecta el clic sobre los elementos y estos despliegan su contenido

--- app/src/main/java/com/android/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposetutorial/MainActivity.kt
@@ -4,17 +4,14 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -24,18 +21,18 @@ import androidx.compose.ui.unit.dp
 import com.android.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 private val messages: List<MyMessage> = listOf(
-    MyMessage("Título 1", "Cuerpo del mensage 1"),
-    MyMessage("Título 2", "Cuerpo del mensage 2"),
-    MyMessage("Título 3", "Cuerpo del mensage 3"),
-    MyMessage("Título 4", "Cuerpo del mensage 4"),
-    MyMessage("Título 5", "Cuerpo del mensage 5"),
-    MyMessage("Título 6", "Cuerpo del mensage 6"),
-    MyMessage("Título 7", "Cuerpo del mensage 7"),
-    MyMessage("Título 8", "Cuerpo del mensage 8"),
-    MyMessage("Título 9", "Cuerpo del mensage 9"),
-    MyMessage("Título 10", "Cuerpo del mensage 10"),
-    MyMessage("Título 11", "Cuerpo del mensage 11"),
-    MyMessage("Título 12", "Cuerpo del mensage 12"),
+    MyMessage("Título 1", "Cuerpo del mensage 1, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 2", "Cuerpo del mensage 2, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages."),
+    MyMessage("Título 3", "Cuerpo del mensage 3, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+    MyMessage("Título 4", "Cuerpo del mensage 4, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 5", "Cuerpo del mensage 5, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 6", "Cuerpo del mensage 6, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 7", "Cuerpo del mensage 7, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 8", "Cuerpo del mensage 8, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 9", "Cuerpo del mensage 9, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 10", "Cuerpo del mensage 10, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 11", "Cuerpo del mensage 11, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Título 12", "Cuerpo del mensage 12, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
 )
 
 
@@ -89,13 +86,19 @@ fun MyImage() {
 
 @Composable
 fun MyTexts(message: MyMessage) {
-    Column(modifier = Modifier.padding(start = 8.dp, top = 8.dp)) {
+
+    var expanded by remember{ mutableStateOf(false)}
+
+    Column(modifier = Modifier.padding(start = 8.dp, top = 8.dp).clickable {
+        expanded = !expanded
+    }) {
         MyText(text = message.title, MaterialTheme.colors.primary, MaterialTheme.typography.subtitle1)
         Spacer(modifier = Modifier.height(8.dp))
         MyText(
             text = message.body,
             MaterialTheme.colors.onBackground,
-            MaterialTheme.typography.subtitle2
+            MaterialTheme.typography.subtitle2,
+            if (expanded) Int.MAX_VALUE else 1
         )
     }
 
@@ -103,8 +106,8 @@ fun MyTexts(message: MyMessage) {
 }
 
 @Composable
-fun MyText(text: String, color: Color, style: androidx.compose.ui.text.TextStyle) {
-    Text(text, color = color, style = style)
+fun MyText(text: String, color: Color, style: androidx.compose.ui.text.TextStyle, lines:Int = Int.MAX_VALUE) {
+    Text(text, color = color, style = style, maxLines = lines)
 }
 
 @Preview(showSystemUi = true)
